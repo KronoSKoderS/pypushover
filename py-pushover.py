8349c4a9a9ad7cbd78296f71e17142994537e015
KronosKoders <superuser.kronos@gmail.com>
2015-08-24 10:55:39 -0700

    Commenting

--- py-pushover.py
+++ py-pushover.py
@@ -1,16 +1,18 @@
-import sys
-#Making compatible for Python 3 and 2.
-if sys.version_info < (3, 0, 0):
-    import urllib
-else:
-    import urllib2.parse as urllib
+import urllib
 
-import urllib2
+try:
+    import urllib.request as urllib_request
+except ImportError:
+    import urllib2 as urllib_request
 
 
 class Sounds(object):
     """
-    Sounds - collection of Push Notifcation sounds
+    Sounds - Collection of Push Notification sounds.
+
+    This is to be used with the 'sounds' argument of the PushOverManager.push_notification method.
+
+    see also: https://pushover.net/api#sounds
     """
     Short_Pushover = 'pushover'
     Short_Bike = 'bike'
@@ -37,43 +39,68 @@ class Sounds(object):
 
 class Priority(object):
     """
-    Priority - collection of Priorities
+    Priority - Collection of Priorities.
+
+    This is to be used with the 'priority' arguemnt of the PushOverManager.push_notification method.
+
+    see also: https://pushover.net/api#priority
     """
-    Lowest = -2
-    Low = -1
-    Normal = 0
-    High = 1
-    Emergency = 2
+    Lowest = -2     # no notification/alert
+    Low = -1        # quiet notification
+    Normal = 0      # normal
+    High = 1        # high-priority (bypass user's quiet hours)
+    Emergency = 2   # require confirmation from the user
 
 
 class PushOverManager(object):
     """
-    Push Over Manager - Class for configuring and sending push notifications
+    Push Over Manager - Class for interfacing with the Pushover API.
+
+    Properties:
+    -----------
+
+
+    Methods:
+    --------
+    push_notificaiton - pushes a notification to a device, user or group.
     """
     _push_url = "https://api.pushover.net/1/messages.json"
     _user_verify_url = "https://api.pushover.net/1/users/validate.json"
-    
+
 
     def __init__(self, app_token, group_key):
         self._app_token = app_token
         self._group_key = group_key
 
-    def push_notification(self, title, message, **kwargs):
+    def push_notification(self, message, **kwargs):
         """
-        :param title:
-        :param message:
-        :param kwargs:
-        :return:
+        :param str message: your message
+        :param str title: your message's title, otherwise your app's name is used
+        :param str device: your user's device name to send the message directly to that device
+        :param list device: your user's devices names to send the message directly to that device
+        :param str url: a supplementary URL to show with your message
+        :param str url_title: a title for your supplementary URL, otherwise just the URL is shown
+        :param int priority: message priority
+        :param int timestamp: a Unix timestamp of your message's date and time to display to the user
+        :param str sound: the name of
+
+        See also: https://pushover.net/api#messages
         """
         json_out = {
             'token': self._app_token,
             'user': self._group_key,
-            'title': title,
             'message': message
         }
 
         # Support for non-required parameters of PushOver
+        if 'title' in kwargs:
+            json_out['title'] = kwargs['title']
         if 'device' in kwargs:
+            temp = kwargs['device']
+            if type(temp) == list:
+                json_out['device'] = ','.join(temp)
+            else:
+                json_out['device'] = temp
             json_out['device'] = kwargs['device']
         if 'url' in kwargs:
             json_out['url'] = kwargs['url']
@@ -87,15 +114,18 @@ class PushOverManager(object):
             json_out['sound'] = kwargs['sound']
 
         data = urllib.urlencode(json_out)
-        req = urllib2.Request(self._push_url, data)
-        if self._response_check(urllib2.urlopen(req)) < 1:
+        req = urllib_request.Request(self._push_url, data)
+
+        if self._response_check(urllib_request.urlopen(req)) < 1:
             raise UserWarning("Notification did not succeed")
 
     @staticmethod
     def _response_check(response):
         """
-        :param response:
+        :param Request response: response from server
         :return:
+
+        TODO: Make more robust
         """
         if response.code == 200:
             return 1

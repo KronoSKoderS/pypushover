f880545ae7323cd85f79ddf8891f4a69f4906abf
KronosKoders <superuser.kronos@gmail.com>
2015-08-26 09:29:51 -0700

    Starting a simple test

--- tests/helpers/__init__.py
+++ tests/helpers/__init__.py
@@ -1,4 +1,4 @@
 __author__ = 'james.mertz'
-from keys import user_key, group_key
+from keys import user_key, group_key, app_key
 
-__all__ = ['user_key', 'group_key']
+__all__ = ['user_key', 'group_key', 'app_key']

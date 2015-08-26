ec188bec1becde9bc69a7c28bd091b3fb08edbf4
KronosKoders <superuser.kronos@gmail.com>
2015-08-26 09:57:10 -0700

    Handling of HTTP error codes are proper now.  Updated testcase to test invalid application key.

--- tests/runtests.py
+++ tests/runtests.py
@@ -2,13 +2,23 @@ import unittest
 from py_pushover import py_pushover as py_po
 from helpers import user_key, group_key, app_key
 
+try:  # Python 3
+    import urllib.request as urllib_request
+    from urllib.parse import urlencode as urllib_encode
+
+except ImportError:  # Python 2
+    import urllib2 as urllib_request
+    from urllib import urlencode as urllib_encode
+
 
 class TestPushManager(unittest.TestCase):
     def setUp(self):
-        self.valid_pm = py_po.PushOverManager(app_key, group_key)
+        self.valid_pm = py_po.PushOverManager(app_key, user_key)
 
-    def test_push_msg(self):
-        self.valid_pm.push_message('test', title='Hello', device='KronoDroid')
+    def test_inv_app_token(self):
+        inv_pm = py_po.PushOverManager(group_key, user_key)
+        with self.assertRaises(urllib_request.HTTPError):
+            inv_pm.push_message('This should never work')
 
 
 if __name__ == "__main__":

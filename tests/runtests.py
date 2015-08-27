e98e82ce18e4827eb6e62362f319bb8430aec8dc
KronosKoderS <mertz.james@gmail.com>
2015-08-26 23:07:11 -0700

    Updated cancel retries and tests.

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 from py_pushover import py_pushover as py_po
 from helpers import user_key, group_key, app_key
 
@@ -29,6 +30,32 @@ class TestPushManager(unittest.TestCase):
     def test_inv_user(self):
         inv_user_key = "justabunchofjunk"
         self.assertFalse(self.valid_pm.validate_user(inv_user_key))
+
+    def test_val_group(self):
+        self.assertTrue(self.valid_pm.validate_group(group_key))
+
+    def test_inv_group(self):
+        inv_group_key = "justabunchofjunk"
+        self.assertFalse(self.valid_pm.validate_group(inv_group_key))
+
+    def test_inv_emergency_msg(self):
+        with self.assertRaises(TypeError):
+            self.valid_pm.push_message('Emergency!', priority=py_po.Priority.Emergency)
+
+        with self.assertRaises(TypeError):
+            self.valid_pm.push_message('Emergency', priority=py_po.Priority.Emergency, retry=30)
+
+        with self.assertRaises(TypeError):
+            self.valid_pm.push_message('Emergency', priority=py_po.Priority.Emergency, expire=3600)
+
+    def test_val_emergency_msg(self):
+        self.valid_pm.push_message("Emergency", priority=py_po.Priority.Emergency, retry=30, expire=3600)
+        time.sleep(5)
+        self.valid_pm.cancel_retries()
+
+
+
+
 
 if __name__ == "__main__":
     unittest.main()

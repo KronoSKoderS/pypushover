6d72a8399a6d7a252c1357a750e69f30274c0ad3
KronosKoderS <mertz.james@gmail.com>
2015-10-23 17:38:59 -0700

    Finalized Group API

--- py_pushover/pushover.py
+++ py_pushover/pushover.py
@@ -230,9 +230,11 @@ class PushOverManager(object):
 
     def group_info(self, group_key=None):
         """
+        Fetches the group name and a list of users subscribed to the group.
 
-        required params: token
-        :return:
+        :param str group_key: The key identifying the group.  If none provided the
+        classes group key is used.
+        :return: A dictionary representing the json response.
         """
         group = None
 
@@ -246,46 +248,121 @@ class PushOverManager(object):
         self._send(self._group_info_url.format(group_key=group), get_method=True)
         return self.latest_response_json
 
-    def group_add_user(self):
+    def group_add_user(self, user, group_key=None, device=None, memo=None):
         """
 
-        required params: token, user
+        required params: user
         optional params: device, memo
         :return:
         """
-        raise NotImplementedError
 
-    def group_remove_user(self):
+        group = None
+
+        if group_key:
+            group = group_key
+        elif self._group_key:
+            group = self._group_key
+        else:
+            raise ValueError("A group key must be supplied")
+
+        param_data = {
+            'user': user
+        }
+
+        if device:
+            param_data['device'] = device
+        if memo:
+            param_data['memo'] = memo
+
+        self._send(self._group_add_user_url.format(group_key=group), param_data)
+
+    def group_remove_user(self, user, group_key=None):
+        """
+
+        required params: user
+        :return:
+        """
+
+        group = None
+
+        if group_key:
+            group = group_key
+        elif self._group_key:
+            group = self._group_key
+        else:
+            raise ValueError("A group key must be supplied")
+
+        param_data = {
+            'user': user
+        }
+
+        self._send(self._group_del_user_url.format(group_key=group), param_data)
+
+    def group_disable_user(self, user, group_key=None):
+        """
+
+        required params: user
+        :return:
+        """
+
+        group = None
+
+        if group_key:
+            group = group_key
+        elif self._group_key:
+            group = self._group_key
+        else:
+            raise ValueError("A group key must be supplied")
+
+        param_data = {
+            'user': user
+        }
+
+        self._send(self._group_dis_user_url.format(group_key=group), param_data)
+
+    def group_enable_user(self, user, group_key=None):
         """
 
         required params: token, user
         :return:
         """
-        raise NotImplementedError
 
-    def group_disable_user(self):
-        """
+        group = None
 
-        required params: token, user
-        :return:
-        """
-        raise NotImplementedError
+        if group_key:
+            group = group_key
+        elif self._group_key:
+            group = self._group_key
+        else:
+            raise ValueError("A group key must be supplied")
 
-    def group_enable_user(self):
-        """
+        param_data = {
+            'user': user
+        }
 
-        required params: token, user
-        :return:
-        """
-        raise NotImplementedError
+        self._send(self._group_ena_user_url.format(group_key=group), param_data)
 
-    def group_rename(self):
+    def group_rename(self, name, group_key=None):
         """
 
         required params: token, name
         :return:
         """
-        raise NotImplementedError
+
+        group = None
+
+        if group_key:
+            group = group_key
+        elif self._group_key:
+            group = self._group_key
+        else:
+            raise ValueError("A group key must be supplied")
+
+        param_data = {
+            'name': name
+        }
+
+        self._send(self._group_ren_url.format(group_key=group), param_data)
 
     def _send(self, url, data_out=None, check_response=True, get_method=False):
         """

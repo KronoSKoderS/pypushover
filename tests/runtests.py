6ae50cc2cf4d7d724951089b9ae8708cfb978208
KronosKoderS <mertz.james@gmail.com>
2015-10-27 16:53:51 -0700

    Seperated tests and testsuites

--- tests/runtests.py
+++ tests/runtests.py
@@ -12,31 +12,13 @@ except ImportError:  # support for Travis CI
     user_key = os.environ['user_key']
 
 
-class TestPushManager(unittest.TestCase):
+class TestMessage(unittest.TestCase):
     def setUp(self):
-        self.valid_pm = py_po.PushOverManager(app_key, user_key, group_key=group_key)
+        self.valid_pm = py_po.message.MessageManager(app_key)
 
-    def test_inv_app_token(self):
-        inv_pm = py_po.PushOverManager(group_key, user_key)
-        with self.assertRaises(requests.HTTPError):
-            inv_pm.push_message('This should never work')
-
-    def test_val_app_token(self):
+    def test_val_msg(self):
         self.valid_pm.push_message('This should always work')
-
-    def test_val_user(self):
-        self.assertTrue(self.valid_pm.validate_user(user_key))
-
-    def test_inv_user(self):
-        inv_user_key = "justabunchofjunk"
-        self.assertFalse(self.valid_pm.validate_user(inv_user_key))
-
-    def test_val_group(self):
-        self.assertTrue(self.valid_pm.validate_group(group_key))
-
-    def test_inv_group(self):
-        inv_group_key = "justabunchofjunk"
-        self.assertFalse(self.valid_pm.validate_group(inv_group_key))
+        py_po.message.push_message(app_key, user_key, 'This should always work')
 
     def test_inv_emergency_msg(self):
         with self.assertRaises(TypeError):
@@ -53,11 +35,16 @@ class TestPushManager(unittest.TestCase):
         time.sleep(5)
         self.valid_pm.cancel_retries()
 
+
+class TestGroup(unittest.TestCase):
+    def setUp(self):
+        self.valid_pm = py_po.groups.GroupManager(app_key, group_key)
+
     def test_group_info(self):
-        info = self.valid_pm.group_info(group_key=group_key)
+        info = self.valid_pm.group_info()
         self.assertEqual(info['name'], 'KronoTestGroup')
 
-        info = self.valid_pm.group_info()
+        info = self.valid_pm.group_info(group_key=group_key)
         self.assertEqual(info['name'], 'KronoTestGroup')
 
     def test_group_add_del_user(self):
@@ -85,6 +72,46 @@ class TestPushManager(unittest.TestCase):
         info = self.valid_pm.group_info()
         self.assertEqual(info['name'], 'KronoTestGroup')
 
+
+class TestVerifcation(unittest.TestCase):
+    def setUp(self):
+        self.valid_pm = py_po.verification.VerificationManager(app_key)
+
+    def test_val_user(self):
+        self.assertTrue(self.valid_pm.validate_user(user_key))
+
+    def test_inv_user(self):
+        inv_user_key = "justabunchofjunk"
+        self.assertFalse(self.valid_pm.validate_user(inv_user_key))
+
+    def test_val_group(self):
+        self.assertTrue(self.valid_pm.validate_group(group_key))
+
+    def test_inv_group(self):
+        inv_group_key = "justabunchofjunk"
+        self.assertFalse(self.valid_pm.validate_group(inv_group_key))
+
+
+class TestSubscription(unittest.TestCase):
+    def setUp(self):
+        raise NotImplementedError
+
+
+class TestLicense(unittest.TestCase):
+    def setUp(self):
+        raise NotImplementedError
+
+
+class TestClient(unittest.TestCase):
+    def setUp(self):
+        raise NotImplementedError
+
+
+class TestBasic(unittest.TestCase):
+    def test_inv_app_token(self):
+        inv_pm = py_po.message.MessageManager(group_key, user_key)
+        with self.assertRaises(requests.HTTPError):
+            raise NotImplementedError  # todo: check that raises error and that returned error message is api key issue
 
 if __name__ == "__main__":
     unittest.main()

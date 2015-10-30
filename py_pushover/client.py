05ebf43e8e3b29d52fa3718725c4065e6f6232a1
KronosKoderS <mertz.james@gmail.com>
2015-10-30 09:47:06 -0700

    Added client functionality.

--- py_pushover/client.py
+++ py_pushover/client.py
@@ -1,4 +1,5 @@
 import websocket
+from multiprocessing import Process
 
 from py_pushover import BaseManager, send, base_url
 
@@ -14,8 +15,8 @@ class ClientManager(BaseManager):
 
     def __init__(self, app_token, secret=None, device_id=None):
         super(ClientManager, self).__init__(app_token)
-        self.__secret = secret
-        self.__device_id = device_id
+        self.__secret__ = secret
+        self.__device_id__ = device_id
         self.messages = []
         self._ws_app = websocket.WebSocketApp(
             self._ws_connect_url,
@@ -24,6 +25,16 @@ class ClientManager(BaseManager):
             on_error=self._on_ws_error,
             on_close=self._on_ws_close
         )
+        self.__on_msg_receipt__ = None
+        self.__p__ = None
+
+    @property
+    def secret(self):
+        return self.__secret__
+
+    @property
+    def device_id(self):
+        return self.__device_id__
 
     def login(self, email, password):
         params = {
@@ -31,22 +42,24 @@ class ClientManager(BaseManager):
             'password': password
         }
         self.latest_response_dict = send(self._login_url, data_out=params)
-        self.__secret = self.latest_response_dict['secret']
+        self.__secret__ = self.latest_response_dict['secret']
+        return self.__secret__
 
     def register_device(self, name):
         params = {
-            'secret': self.__secret,
+            'secret': self.__secret__,
             'name': name,
             'os': 'O'
         }
 
         self.latest_response_dict = send(self._register_device_url, data_out=params)
-        self.__device_id = self.latest_response_dict['id']
+        self.__device_id__ = self.latest_response_dict['id']
+        return self.__device_id__
 
     def retrieve_message(self):
         params = {
-            'secret': self.__secret,
-            'device_id': self.__device_id
+            'secret': self.__secret__,
+            'device_id': self.__device_id__
         }
 
         self.latest_response_dict = send(self._message_url, data_out=params, get_method=True)
@@ -54,18 +67,31 @@ class ClientManager(BaseManager):
 
     def clear_server_messages(self):
         params = {
-            'secret': self.__secret,
+            'secret': self.__secret__,
             'message': max([i['id'] for i in self.messages])
         }
 
-        self.latest_response_dict = send(self._del_message_url.format(device_id=self.__device_id), params)
+        self.latest_response_dict = send(self._del_message_url.format(device_id=self.__device_id__), params)
 
     def acknowledge_message(self, receipt):
         params = {
-            'secret': self.__secret
+            'secret': self.__secret__
         }
 
         self.latest_response_dict = send(self._ack_message_url.format(receipt_id=receipt), params)
+
+    def listen(self, on_msg_receipt):
+        self.__on_msg_receipt__ = on_msg_receipt
+        self._ws_app.run_forever()
+
+    def listen_async(self, on_msg_receipt):
+        self.__p__ = Process(target=self.listen, args=(on_msg_receipt,))
+        self.__p__.start()
+
+    def _on_ws_open(self, ws):
+        print("Opening Connection to Pushover Server....")
+        ws.send(self._ws_login.format(device_id=self.__device_id__, secret=self.__secret__))
+        print("Success!")
 
     def _on_ws_message(self, ws, message):
         """
@@ -77,14 +103,26 @@ class ClientManager(BaseManager):
         :param message:
         :return:
         """
-        pass
+        message = message.decode("utf-8")
+        if message == "#":
+            pass
+        elif message == "!":
+            self.retrieve_message()
+            self.__on_msg_receipt__(self.messages)
+
+        elif message == "R":
+            print("Reconnecting to server...")
+            ws.close()
+            self.listen(self.__on_msg_receipt__)
+        elif message == "E":
+            raise NotImplementedError
+        else:
+            raise NotImplementedError
 
     def _on_ws_error(self, ws, error):
-        pass
+        print("Error: " + error)
 
-    def _on_ws_close(self, ws, error):
-        pass
-
-    def _on_ws_open(self, ws):
-        pass
+    def _on_ws_close(self, ws):
+        print("###Connection to Server Closed###")
+        self._ws_app = None
 

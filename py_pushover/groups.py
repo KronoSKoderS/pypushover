b533e06d427f38292d51965ec059391b4e0680e9
KronosKoderS <mertz.james@gmail.com>
2015-10-29 19:32:38 -0700

    Updated Groups Api

--- py_pushover/groups.py
+++ py_pushover/groups.py
@@ -8,147 +8,177 @@ _group_dis_user_url = _group_url + "/disable_user.json"
 _group_ena_user_url = _group_url + "/enable_user.json"
 _group_ren_url = _group_url + "/rename.json"
 
+
 class GroupManager(BaseManager):
     def __init__(self, app_token, group_key):
         super(GroupManager, self).__init__(app_token, group_key=group_key)
-
 
     def info(self):
         """
         Fetches the group name and a list of users subscribed to the group.
 
-        :param str group_key: The key identifying the group.  If none provided the
-        classes group key is used.
         :return: A dictionary representing the json response.
         """
 
         self.latest_response_dict = info(self._app_token, self._group_key)
         return self.latest_response_dict
 
-    def group_add_user(self, user, group_key=None, device=None, memo=None):
+    def group_add_user(self, user, device=None, memo=None):
+        """
+        Adds the selected user to the group
+
+        :param str user: the user id of the user to add
+        :param str device: the associated device name (optional)
+        :param str memo: memo (optional)
+        :return: A dictionary representing the json response.
         """
 
-        required params: user
-        optional params: device, memo
-        :return:
+        self.latest_response_dict = add_user(self._app_token, self._group_key, user, device=device, memo=memo)
+        return self.latest_response_dict
+
+    def group_remove_user(self, user):
+        """
+        Removes the selected user from the group
+        :param str user: the user id of the user to deleted
+        :return: A dictionary representing the json response.
         """
 
-        group = None
+        self.latest_response_dict = remove_user(self._app_token, self._group_key, user)
+        return self.latest_response_dict
 
-        if group_key:
-            group = group_key
-        elif self._group_key:
-            group = self._group_key
-        else:
-            raise ValueError("A group key must be supplied")
-
-        param_data = {
-            'user': user
-        }
-
-        if device:
-            param_data['device'] = device
-        if memo:
-            param_data['memo'] = memo
-
-        self._send(self._group_add_user_url.format(group_key=group), param_data)
-
-    def group_remove_user(self, user, group_key=None):
+    def group_disable_user(self, user):
+        """
+        Disables the user from receiving notifications sent to the group
+        :param str user: the user id of the user to disable
+        :return: A dictionary representing the json response.
         """
 
-        required params: user
-        :return:
+        self.latest_response_dict = disable_user(self._app_token, self._group_key, user)
+        return self.latest_response_dict
+
+    def group_enable_user(self, user):
+        """
+        Enables the user to receive notifications sent to the group
+        :param str user: the user id of the user to enable
+        :return: A dictionary representing the json response.
         """
 
-        if group_key:
-            group = group_key
-        elif self._group_key:
-            group = self._group_key
-        else:
-            raise ValueError("A group key must be supplied")
+        self.latest_response_dict = enable_user(self._app_token, self._group_key, user)
+        return self.latest_response_dict
 
-        param_data = {
-            'user': user
-        }
-
-        self._send(self._group_del_user_url.format(group_key=group), param_data)
-
-    def group_disable_user(self, user, group_key=None):
+    def group_rename(self, name):
+        """
+        Renames the group
+        :param str name: the name of the group to change to
+        :return: A dictionary representing the json response.
         """
 
-        required params: user
-        :return:
-        """
+        self.latest_response_dict = rename(self._app_token, self._group_key, name)
+        return self.latest_response_dict
 
-        if group_key:
-            group = group_key
-        elif self._group_key:
-            group = self._group_key
-        else:
-            raise ValueError("A group key must be supplied")
-
-        param_data = {
-            'user': user
-        }
-
-        self._send(self._group_dis_user_url.format(group_key=group), param_data)
-
-    def group_enable_user(self, user, group_key=None):
-        """
-
-        required params: token, user
-        :return:
-        """
-
-        group = None
-
-        if group_key:
-            group = group_key
-        elif self._group_key:
-            group = self._group_key
-        else:
-            raise ValueError("A group key must be supplied")
-
-        param_data = {
-            'user': user
-        }
-
-        self._send(self._group_ena_user_url.format(group_key=group), param_data)
-
-    def group_rename(self, name, group_key=None):
-        """
-
-        required params: token, name
-        :return:
-        """
-
-        if group_key:
-            group = group_key
-        elif self._group_key:
-            group = self._group_key
-        else:
-            raise ValueError("A group key must be supplied")
-
-        param_data = {
-            'name': name
-        }
-
-        self._send(self._group_ren_url.format(group_key=group), param_data)
 
 def info(app_token, group):
-    return send(_group_info_url.format(group_key=group), params={'token':app_token}, get_method=True)
+    """
+    Fetches the group name and a list of users subscribed to the group.
 
-def add_user(app_token, group, user):
-    raise NotImplementedError
+    :param str app_token: your applications token
+    :param str group: the group id to return info on
+    :return: A dictionary representing the json response.
+    """
+    param_data = {
+        'token': app_token,
+    }
+
+    return send(_group_info_url.format(group_key=group), param_data, get_method=True)
+
+
+def add_user(app_token, group, user, device=None, memo=None):
+    """
+    Adds the selected user to the group
+
+    :param str app_token: your applications token
+    :param str group: the group id
+    :param str user: the user id of the user to add
+    :param str device: the associated device name (optional)
+    :param str memo: memo (optional)
+    :return: A dictionary representing the json response.
+    """
+
+    param_data = {
+        'token': app_token,
+        'user': user
+    }
+
+    if device:
+        param_data['device'] = device
+    if memo:
+        param_data['memo'] = memo
+
+    return send(_group_add_user_url.format(group_key=group), param_data)
+
 
 def remove_user(app_token, group, user):
-    raise NotImplementedError
+    """
+    Removes the selected user from the group
+    :param str app_token: your applications token
+    :param str group: the group id
+    :param str user: the user id of the user to deleted
+    :return: A dictionary representing the json response.
+    """
+
+    param_data = {
+        'token': app_token,
+        'user': user
+    }
+
+    return send(_group_del_user_url.format(group_key=group), param_data)
+
 
 def disable_user(app_token, group, user):
-    raise NotImplementedError
+    """
+    Disables the user from receiving notifications sent to the group
+    :param str app_token: your applications token
+    :param str group: the group id
+    :param str user: the user id of the user to disable
+    :return: A dictionary representing the json response.
+    """
+
+    param_data = {
+        'token': app_token,
+        'user': user
+    }
+
+    return send(_group_dis_user_url.format(group_key=group), param_data)
+
 
 def enable_user(app_token, group, user):
-    raise NotImplementedError
+    """
+    Enables the user to receive notifications sent to the group
+    :param str app_token: your applications token
+    :param str group: the group id
+    :param str user: the user id of the user to enable
+    :return: A dictionary representing the json response.
+    """
+    param_data = {
+        'token': app_token,
+        'user': user
+    }
 
-def rename(app_token, group, user)
-    raise NotImplementedError
+    return send(_group_ena_user_url.format(group_key=group), param_data)
+
+
+def rename(app_token, group, name):
+    """
+    Renames the group
+    :param str app_token: your applications token
+    :param str group: the group id
+    :param str name: the name of the group to change to
+    :return: A dictionary representing the json response.
+    """
+
+    param_data = {
+        'token': app_token,
+        'name': name
+    }
+
+    return send(_group_ren_url.format(group_key=group), param_data)

4b1f6a5b776f4f7e8b186d55b41db89799a9967b
KronosKoderS <mertz.james@gmail.com>
2015-10-31 02:32:04 -0700

    Updated documentation

--- py_pushover/_base.py
+++ py_pushover/_base.py
@@ -19,6 +19,15 @@ class BaseManager(object):
 
 
 def send(url, data_out=None, get_method=False):
+    """
+    Sends a request to the selected url with the payload `data_out`.  Set `get_method` to True to send as a GET request.
+    Default request is a POST.
+
+    :param str url: url to send the request to
+    :param dict data_out: payload data to send
+    :param bool get_method: True = GET request; False = POST request (default)
+    :return dict: a dictionary with the json results of the request.
+    """
     if get_method:
         res = requests.get(url, params=data_out)
     else:

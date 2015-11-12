033b5b4f339b362006df781e637eaec6e477adcd
KronosKoderS <mertz.james@gmail.com>
2015-11-12 06:59:09 -0700

    Adding dynamic sounds

--- py_pushover/Constants/SOUNDS.py
+++ py_pushover/Constants/SOUNDS.py
@@ -5,6 +5,10 @@ This is to be used with the 'sounds' argument of the PushOverManager.push_notifi
 
 see also: https://pushover.net/api#sounds
 """
+from py_pushover._base import  send
+
+_sounds_url = "https://api.pushover.net/1/sounds.json"
+
 SHORT_PUSHOVER = 'pushover'
 SHORT_BIKE = 'bike'
 SHORT_BUGLE = 'bugle'
@@ -26,3 +30,14 @@ LONG_PERSISTENT = 'persistent'
 LONG_PUSHOVER_ECHO = 'echo'
 LONG_UP_DOWN = 'updown'
 SILENT = 'none'
+
+
+class CurrentSounds(object):
+    """
+    Dynamic class for selecting sounds directly queried from the Pushover Api.
+    """
+    def __init__(self, app_token):
+        res = send(_sounds_url, {'token': app_token})
+        for k, v in res['sounds'].items():
+            p = v.replace('(', '').replace(')', '').replace(' ', '_')
+            setattr(self, p, k)

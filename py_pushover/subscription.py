599d99884ea7b22884f005774cccad95824b2605
KronosKoderS <mertz.james@gmail.com>
2015-11-12 07:28:45 -0700

    Added Subscription API

--- py_pushover/subscription.py
+++ py_pushover/subscription.py
@@ -1,9 +1,22 @@
-from py_pushover import BaseManager
+from py_pushover import BaseManager, send
+
+_subscription_url = "https://pushover.net/subscribe/"
 
 
-class SubscriptionManager(BaseManager):
-    def __init__(self, app_token, user_key, sub_code):
-        super().__init__(app_token, user_key=user_key)
-        self._sub_code = sub_code
-        raise NotImplementedError
+class SubscriptionManager(object):
+    def __init__(self, sub_code):
+        if sub_code.startswith('https://'):
+            self._sub_code = sub_code
+        else:
+            self._sub_code = _subscription_url + sub_code
 
+    def subscribe(self, sucess, failure):
+        subscribe(self._sub_code, sucess, failure)
+
+
+def subscribe(url, success, failure):
+    params = {
+        'success': success,
+        'failure': failure
+    }
+    send(url, data_out=params)
